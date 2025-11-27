cf6f13174ab3bbdce776452c73f4fe2b54a291cc
gabriele barletta <gabriele.barletta03@gmail.com>
2025-11-27 18:41:13 +0100

    aggiornamento

--- lidar_ws/build/helios16p_cane_robot/prefix_override/sitecustomize.py
+++ lidar_ws/build/helios16p_cane_robot/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ph/ws/lidar_ws_principale/install/helios16p_cane_robot'
+    sys.prefix = sys.exec_prefix = '/home/ph/ws/lidar_ws/install/helios16p_cane_robot'
