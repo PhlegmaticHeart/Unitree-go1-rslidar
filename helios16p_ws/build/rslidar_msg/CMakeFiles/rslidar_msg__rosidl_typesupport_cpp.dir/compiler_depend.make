6af8b46378a357f8dc7b46f672f4e056926837b4
gabriele barletta <gabriele.barletta03@gmail.com>
2026-01-15 16:17:19 +0100

    New yaml configs; New go urdf model

--- helios16p_ws/build/rslidar_msg/CMakeFiles/rslidar_msg__rosidl_typesupport_cpp.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for rslidar_msg__rosidl_typesupport_cpp.
-# This may be replaced when dependencies are built.
--- helios16p_ws/src/helios16p_cane_robot/launch/helios16p_cane_robot.launch.py
+++ helios16p_ws/src/helios16p_cane_robot/launch/helios16p_cane_robot.launch.py
@@ -13,22 +13,22 @@ package_name = 'helios16p_cane_robot'
 def generate_launch_description():
 
 # Files' path retrieval
-    pkg_share = FindPackageShare('helios16p_cane_robot').find('helios16p_cane_robot')
+    pkg_share = FindPackageShare('helios16p_cane_robot').find('helios16p_cane_robot') # Package share directory
 
-    default_config_file_path = os.path.join(
+    default_config_file_path = os.path.join( # Default Kiss-ICPconfiguration file path
         get_package_share_directory(package_name), 'configs', 'config_mrange100_voxsize5Em1_voxpoints20.yaml'
     )
     
-    custom_config_file_path = os.path.join(
+    custom_config_file_path = os.path.join( # Custom configuration file path
         get_package_share_directory(package_name), 'configs', 'config_mrange30_voxsize3Em1_voxpoints10.yaml'
     )
 
-    xacro_file = os.path.join(pkg_share, 'urdf', 'helios16p.urdf.xacro')
-    robot_description = xacro.process_file(xacro_file).toxml()
+    xacro_file = os.path.join(pkg_share, 'urdf', 'helios16p.urdf.xacro') # Robot's xacro file path
+    robot_description = xacro.process_file(xacro_file).toxml() # Robot description in xml format, keep in mind that this model requires xacro package
 
-    lidar_config_file = os.path.join(pkg_share, 'configs', 'lidar_config.yaml')
+    lidar_config_file = os.path.join(pkg_share, 'configs', 'lidar_config.yaml') # RSLidar's config file path
 
-    bag_path = '/home/ph/bagrecords/bagtest600/rosbag2_600_BIG/'
+    bag_path = '/home/ph/bagrecords/bagtest600/rosbag2_600_BIG/' # Bag file path
 
 # Simulation and bag flags
     simulate = LaunchConfiguration('simulate', default=False) # Flag simulation time + bag
@@ -37,18 +37,18 @@ def generate_launch_description():
     bagfile = LaunchConfiguration('bagfile', default=bag_path) # Bag file path
 
 # Kiss-ICP's debug parameters
-    visualize = LaunchConfiguration('visualize', default=False)
+    visualize = LaunchConfiguration('visualize', default=False) # Flag to enable rviz visualization
     max_range = LaunchConfiguration('data.max_range', default=30.0) # By default its 100
     min_range = LaunchConfiguration('data.min_range', default=0.2) # By default its 0
-    mapping_voxel_size = LaunchConfiguration('mapping.voxel_size', default=0.1) # By default its 0.5
+    mapping_voxel_size = LaunchConfiguration('mapping.voxel_size', default=0.3) # By default its 0.5
     mapping_voxel_points = LaunchConfiguration('mapping.max_points_per_voxel', default=10) # By default its 20
     data_deskew = LaunchConfiguration('data.deskew', default=True) # Enable manipulation of max and min range
     base_frame = LaunchConfiguration('base_frame', default='')  # (base_link/base_footprint)
-    lidar_odom_frame = LaunchConfiguration('lidar_odom_frame', default='odom_lidar')
-    publish_odom_tf = LaunchConfiguration('publish_odom_tf', default=True)
-    invert_odom_tf = LaunchConfiguration('invert_odom_tf', default=True)
-    max_num_iterations = LaunchConfiguration('registration.max_num_iterations', default=500) # By default its 500
-    convergence_criterion = LaunchConfiguration('registration.convergence_criterion', default=0.0001) # By default its 0.0001
+    lidar_odom_frame = LaunchConfiguration('lidar_odom_frame', default='odom_lidar') # It gives the name to the frame of the odometry published by kiss-ICP
+    publish_odom_tf = LaunchConfiguration('publish_odom_tf', default=True) # Flag to publish odom->base_link transform
+    invert_odom_tf = LaunchConfiguration('invert_odom_tf', default=True) # necessary for getting the correct transform as by default kiss-ICP pblishes an inverted transform
+    max_num_iterations = LaunchConfiguration('registration.max_num_iterations', default=700) # By default its 500
+    convergence_criterion = LaunchConfiguration('registration.convergence_criterion', default=0.000001) # By default its 0.0001
 
 # Topics' configuration
     declare_topic_arg = DeclareLaunchArgument(
@@ -63,7 +63,7 @@ def generate_launch_description():
 
         declare_topic_arg,
 
-        Node(
+        Node( # Publish robot state
             package='robot_state_publisher',
             executable='robot_state_publisher',
             output='screen',
@@ -72,7 +72,7 @@ def generate_launch_description():
                 'use_sim_time': use_sim_time,
             }],
         ),
-        Node(
+        Node( # Static transform from map to base_link
             package='tf2_ros',
             name='static_transform_map_to_base_link',
             executable='static_transform_publisher',
@@ -80,7 +80,7 @@ def generate_launch_description():
             parameters=[{'use_sim_time' : use_sim_time}],
             output='screen',
         ),
-        Node(
+        Node( # Static transform from base_link to rslidar
             package='tf2_ros',
             name='static_transform_base_link_to_rslidar',
             executable='static_transform_publisher',
@@ -89,7 +89,7 @@ def generate_launch_description():
             output='screen',
         ),
 
-        ExecuteProcess(
+        ExecuteProcess( # Bag file playback
             cmd=[
                 'ros2', 'bag', 'play',
                 '--rate', '0.5',
@@ -102,7 +102,7 @@ def generate_launch_description():
             name='startmybag',
         ),
 
-        Node(
+        Node( # Driver node
             package='rslidar_sdk',
             executable='rslidar_sdk_node',
             name='helios16p_node',
@@ -111,7 +111,7 @@ def generate_launch_description():
             condition=UnlessCondition(play_bag),
         ),
 
-        Node(
+        Node( # kiss-ICP node
             package='kiss_icp',
             executable='kiss_icp_node',
             name='kiss_icp_node',
@@ -122,9 +122,9 @@ def generate_launch_description():
             parameters=[
 
 
-            default_config_file_path, # Default configuration file
-
-            #custom_config_file_path, # Custom configuration file
+            default_config_file_path,   #
+                                        # Main configurations, just de-flag the desired one and flag the other
+            #custom_config_file_path,   #
 
 
                 {#///kiss-ICP's configuration\\\
@@ -139,12 +139,12 @@ def generate_launch_description():
 
                 # Main parameters to tune for performance evaluation
 
-                #  'data.max_range': max_range,
-                #  'data.min_range': min_range,
-                #  'mapping.max_points_per_voxel': mapping_voxel_points,
-                #  'mapping.voxel_size': mapping_voxel_size,
-                #  'registration.convergence_criterion': convergence_criterion,
-                #  'registration.max_num_iterations': max_num_iterations,
+                 'data.max_range': max_range,
+                 'data.min_range': min_range,
+                 'mapping.max_points_per_voxel': mapping_voxel_points,
+                 'mapping.voxel_size': mapping_voxel_size,
+                 'registration.convergence_criterion': convergence_criterion,
+                 'registration.max_num_iterations': max_num_iterations,
 
                 # More parameters available for tuning in the configuration files
 
@@ -158,7 +158,7 @@ def generate_launch_description():
         ),
 
 
-        Node(
+        Node( # Rviz2 node
             package='rviz2',
             executable='rviz2',
             output='screen',
