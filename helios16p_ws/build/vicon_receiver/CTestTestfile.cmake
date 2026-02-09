748e4f780e4f26aba32d72c0316ce93cf8f04b4f
gbarletta <gabriele.barletta03@gmail.com>
2026-02-09 11:14:22 +0100

    update

--- helios16p_ws/build/vicon_receiver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ph/treasure/dockerBENCH/helios16p_ws/src/vicon_receiver
-# Build directory: /home/ph/treasure/dockerBENCH/helios16p_ws/build/vicon_receiver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
