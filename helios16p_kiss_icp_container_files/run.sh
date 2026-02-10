c12d040dee8e5909fa3f6e732aefbe75e3da4fac
gbarletta <gabriele.barletta03@gmail.com>
2026-02-10 13:00:15 +0100

    [Containerization mods]: Dockerfile entrypoint for kiss-icp pipeline autostart added; run.sh file optimized for plug and play use.

--- helios16p_kiss_icp_container_files/run.sh
+++ helios16p_kiss_icp_container_files/run.sh
@@ -9,11 +9,14 @@ TAG="latest"
 PLATFORM=$(docker info --format '{{.Architecture}}')  # Auto-detect
 
 echo "Starting $IMAGE_NAME"
+
 docker run \
   --rm \
   -it \
   --privileged \
   --network=host \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -e DISPLAY=$DISPLAY \
-  "$IMAGE_NAME:$TAG" 
+  "$IMAGE_NAME:$TAG"
+
+# Actually the container dialogue directly with your host connection.
+# The container actually can uses display sharing with the host, to do this exec into you shell [  xhost +local:docker  ] command and add the following parameter to the docker run command into this script [ -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY ] for debugging purposes. comment the related linea for disabling this.
+# After the end of the session, the container will be removed to not waste storage while not in use
