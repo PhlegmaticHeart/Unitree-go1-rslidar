3adaeeb436ad662570d57fcd7327b718afeb78e0
gbarletta <gabriele.barletta03@gmail.com>
2026-02-25 18:20:37 +0100

    [added new exported variables to entrypoint.sh]

--- helios16p_kiss_icp_container_files/entrypoint.sh
+++ helios16p_kiss_icp_container_files/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export CYCLONEDDS_URI=/cyclonedds_config.xml 
+
 bash -c "source /home/admin/.bashrc && source /opt/ros/humble/setup.bash && source /home/admin/helios16p_ws/install/setup.bash && ros2 launch helios16p_cane_robot helios16p_cane_robot.launch.py setrange:=mrange8.yaml visualize_clouds:=true nomap:=true state_publisher:=false"
