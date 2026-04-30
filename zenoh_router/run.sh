cc95cbc2f4674a6b0c2b6b54e2e047a60cf92f4d
gbarletta <gabriele.barletta03@gmail.com>
2026-04-30 15:59:38 +0200

    zenoh_router - [ cleaned formatting of all provided files ].

--- zenoh_router/run.sh
+++ zenoh_router/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+SCRIPTFOLDER=$( dirname $0 )
+
 docker run --name zenoh_router_container \
   --rm -it --network=host \
   -e ROS_DOMAIN_ID=3 \
   -e ROS_DISTRO=humble \
-  --mount type=bind,source=$PWD/entrypoint.sh,target=/entrypoint.sh \
-  --mount type=bind,source=$PWD/cyclonedds_config.xml,target=/cyclonedds_config.xml \
-  --mount type=bind,source=$PWD/entrypoint.sh,target=/entrypoint.sh \
-  --mount type=bind,source=$PWD/zenoh_config_zeppelin.json,target=/zenoh_config_zeppelin.json \
+  -v $SCRIPTFOLDER/entrypoint.sh:/entrypoint.sh \
+  -v $SCRIPTFOLDER/cyclonedds_config.xml:/cyclonedds_config.xml \
+  -v $SCRIPTFOLDER/entrypoint.sh:/entrypoint.sh \
+  -v $SCRIPTFOLDER/zenoh_config_zeppelin.json:/zenoh_config_zeppelin.json \
   --entrypoint "/entrypoint.sh" \
   eclipse/zenoh-bridge-ros2dds:1.8.0
