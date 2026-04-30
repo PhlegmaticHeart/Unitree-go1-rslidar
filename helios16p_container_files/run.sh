ad7ede631b024b4aa304e442240047ce9159f46b
gbarletta <gabriele.barletta03@gmail.com>
2026-04-30 16:35:56 +0200

    [ helios16p_kiss-icp_container improvements ]: moved certain logics to environment varibles, improved formatting.

--- helios16p_container_files/run.sh
+++ helios16p_container_files/run.sh
@@ -2,6 +2,7 @@ set -e
 
 IMAGE_NAME="helios16p_kiss-icp_cyclone"
 TAG="latest"
+SCRIPTFOLDER=$( dirname $0 )
 
 echo "Starting $IMAGE_NAME"
 
@@ -14,11 +15,14 @@ docker run \
   -e DISPLAY=$DISPLAY \
   -e XAUTHORITY=/root/.Xauthority \
   -e ROS_DOMAIN_ID=3 \
-  -e ROS_AUTOMATIC_DISCOVERY_RANGE=SUBNET \
+  -e CYCLONEDDS_URI=/cyclonedds_config.xml \
+  -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
+  -e ROS_DISTRO=humble \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v ~/.Xauthority:/root/.Xauthority \
-  --mount type=bind,source=./entrypoint.sh,target=/usr/bin/entrypoint.sh \
-  --mount type=bind,source=./helios16p_ws,target=/home/admin/debug_build \
-  --entrypoint /usr/bin/entrypoint.sh \
+  -v $SCRIPTFOLDER/entrypoint.sh:/entrypoint.sh \
+  -v $SCRIPTFOLDER/cyclonedds_config.xml:/cyclonedds_config.xml \
+  -v $SCRIPTFOLDER/helios16p_ws:/home/admin/debug_build \
+  --entrypoint /entrypoint.sh \
   "$IMAGE_NAME:$TAG"
 
