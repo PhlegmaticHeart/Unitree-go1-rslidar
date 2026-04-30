1d8b301a2f965d12edbcc104f7dee984d081dae0
gbarletta <gabriele.barletta03@gmail.com>
2026-04-30 16:41:52 +0200

    helios16p_kiss-icp_container - [ launch files improved ]:cleaned, improved formatting; [ added test config files ].

--- helios16p_container_files/helios16p_ws/src/build/helios16p_cane_robot/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/admin/bench/lab/STAGING_helios16p_container_files/helios16p_ws/src/install/helios16p_cane_robot'
