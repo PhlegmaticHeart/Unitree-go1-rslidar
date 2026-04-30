3766717213602835a39c08dc7c4591677234f19d
gbarletta <gabriele.barletta03@gmail.com>
2026-04-30 16:20:06 +0200

    [ Improved zenoh containers ]: moved logics to environment variables, better formatting.

--- zenoh_client/run.sh
+++ zenoh_client/run.sh
@@ -6,7 +6,9 @@ docker run --name zenoh_client_container \
   --rm -it --network=host \
   -e ROS_DOMAIN_ID=7 \
   -e ROS_DISTRO=humble \
-  -e ROS_LOCALHOST_ONLY=0 \
+  -e ROS_LOCALHOST_ONLY=1 \
+  -e CYCLONEDDS_URI=/cyclonedds_config.xml \
+  -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
   -v $SCRIPTFOLDER/entrypoint.sh:/entrypoint.sh \
   -v $SCRIPTFOLDER/cyclonedds_config.xml:/cyclonedds_config.xml \
   -v $SCRIPTFOLDER/zenoh_config_zeppelin.json:/zenoh_config_zeppelin.json \
--- zenoh_router/run.sh
+++ zenoh_router/run.sh
@@ -6,6 +6,8 @@ docker run --name zenoh_router_container \
   --rm -it --network=host \
   -e ROS_DOMAIN_ID=3 \
   -e ROS_DISTRO=humble \
+  -e CYCLONEDDS_URI=/cyclonedds_config.xml \
+  -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
   -v $SCRIPTFOLDER/entrypoint.sh:/entrypoint.sh \
   -v $SCRIPTFOLDER/cyclonedds_config.xml:/cyclonedds_config.xml \
   -v $SCRIPTFOLDER/entrypoint.sh:/entrypoint.sh \
